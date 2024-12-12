633dcd3b7381aca032fe8522dd0c55d8f7cc2e57
Lucas Souza <L.c.s.souzaroots@gmail.com>
2024-12-11 22:03:38 -0300

    ajustando html com JS

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Descubra o numero Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
