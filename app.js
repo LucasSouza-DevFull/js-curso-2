030ff21e6585b73f5b8199ca67a6b86a2080af6a
Lucas Souza <L.c.s.souzaroots@gmail.com>
2025-03-24 05:24:49 -0300

    testes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela (tag, texto){
 
 function exibirMensagemInicial() {
         exibirTextoNaTela('h1', 'Descubra o número Secreto TESTE EFETUADO');
-        exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+        exibirTextoNaTela('p', 'Escolha um número entre 1 e 10 teste 2');
 }
 
 exibirMensagemInicial();
